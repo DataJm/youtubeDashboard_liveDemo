160aeb6bd995f6fb1b9ca1af6f28aaf29ae9b4bf
JM <jm@MacBook-Pro-de-JM-2.local>
2021-02-06 10:47:24 -0600

    STEP 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -10,37 +10,46 @@ def index():
 
 @app.route("/api_views")
 def api_views():
-    connection_string = "postgres://postgres:gs@localhost/youtube"
-    conn = create_engine(connection_string)
-    data = pd.read_sql("select * from ratings",conn)
+    # connection_string = "postgres://postgres:gs@localhost/youtube"
+    # conn = create_engine(connection_string)
+    # data = pd.read_sql("select * from ratings",conn)
 
-    resultado = data.groupby("channel_title")["views","likes"].sum()
+    # resultado = data.groupby("channel_title")["views","likes"].sum()
 
-    return (
-        resultado
-        .sort_values(by="views", ascending=False)
-        .reset_index()
-        .loc[:,["channel_title","views"]]
-        .head()
-        .to_json(orient="records")
-    )
+    # return (
+    #     resultado
+    #     .sort_values(by="views", ascending=False)
+    #     .reset_index()
+    #     .loc[:,["channel_title","views"]]
+    #     .head()
+    #     .to_json(orient="records")
+    # )
+    return jsonify([{
+        "channel_title":"demo",
+        "views": 100
+    }])
 
 @app.route("/api_likes")
 def api_likes():
-    connection_string = "postgres://postgres:gs@localhost/youtube"
-    conn = create_engine(connection_string)
-    data = pd.read_sql("select * from ratings",conn)
+    # connection_string = "postgres://postgres:gs@localhost/youtube"
+    # conn = create_engine(connection_string)
+    # data = pd.read_sql("select * from ratings",conn)
 
-    resultado = data.groupby("channel_title")["views","likes"].sum()
+    # resultado = data.groupby("channel_title")["views","likes"].sum()
 
-    return (
-        resultado
-        .sort_values(by="likes", ascending=False)
-        .reset_index()
-        .loc[:,["channel_title","likes"]]
-        .head()
-        .to_json(orient="records")
-    )
+    # return (
+    #     resultado
+    #     .sort_values(by="likes", ascending=False)
+    #     .reset_index()
+    #     .loc[:,["channel_title","likes"]]
+    #     .head()
+    #     .to_json(orient="records")
+    # )
+    return jsonify([{
+        "channel_title":"demo",
+        "likes": 100
+    }])
+
 
 if __name__=="__main__":
     app.run(debug=True)
